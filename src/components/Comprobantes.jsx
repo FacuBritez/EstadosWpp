ea393f1b3c6cdd636c10cac3e9d2fc0245bce2ac
FacuBritez <FacundoIvanBritez@gmail.com>
2025-02-16 21:11:17 -0300

    .

--- src/components/Comprobantes.jsx
+++ src/components/Comprobantes.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Comprobantes.css";
 import * as pdfjsLib from "pdfjs-dist/build/pdf";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.js";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 
 function Comprobantes() {
   const [overlayType, setOverlayType] = useState("NaranjaX 1");
